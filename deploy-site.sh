9b1a0113181a268a9d921793a3c3f48118e72252
Robin Marchart <Robin.Marchart@hotmail.com>
2019-05-16 19:02:33 +0200

    added initial site deploy

---
+++ deploy-site.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+rm -rq site
+mkdir site
+git clone $GitRepo site
+cd site
+git checkout gh-pages
+cd ..
+./gradlew projectReport check javadoc
+if grep -Fxq site/branches.txt $TRAVIS_COMMIT
+echo $TRAVIS_COMMIT >>site/branches.txt
+fi
+cd site
+git commit --amend -a
