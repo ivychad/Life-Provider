27ce3bccaf184732e96bef8f77ee6dffb9587874
Robin Marchart <Robin.Marchart@hotmail.com>
2019-05-21 20:59:42 +0200

    escaping problems

--- injected.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $("a[href]").filter(function (index, element) {
-        return !element.attr("href").includes(location.host);
-    }).attr({
-        target: "_blank",
-        rel: "noopener noreferrer"
-    });
-});
